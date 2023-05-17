27a936faa561e2cfd3ad32291ebe8f7b25bca1da
l-huisman <huisman.l@yahoo.com>
2023-05-17 08:44:31 +0200

    Create todo component added

---
+++ app/create-todo.tsx
@@ -0,0 +1,8 @@
+export default function CreateTodo() {
+    return (
+        <>
+            <h2>Create Todo</h2>
+            <div><input type="text" /><button>Add</button></div>
+        </>
+    )
+}
